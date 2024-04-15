01e2b2c446bb35fb3776e0717e95650b61728574
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2024-04-15 13:13:06 -0600

    added files
    added files and did some start up code to start project

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      fontFamily: {
+        'permanent-marker-regular': ['Permanent Marker', 'cursive'], // Define 'permanent-marker-regular' font family
+        sans: ['Permanent Marker', 'cursive'], // Use 'Permanent Marker' font for sans-serif
+      },
+      letterSpacing: {
+        widest: '.3em',
+      },
+    },
+  },
+  plugins: [],
+};
